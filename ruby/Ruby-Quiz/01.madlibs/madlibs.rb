a54566289331bc323ae44644b3b3091e929bf7aa
Aurélien Bottazini <abottazini@gmail.com>
2009-12-01 17:53:00 +0100

    Ruby Quizz: madlibs seems to work

--- ruby/Ruby-Quiz/01.madlibs/madlibs.rb
+++ ruby/Ruby-Quiz/01.madlibs/madlibs.rb
@@ -4,24 +4,37 @@ class Madlibs
   def initialize(filepath)
     @madlib = File.read(filepath)
     @words = @madlib.scan(/\(\([^(\)\))]+\)\)/)
-    @answers = Hash.new
+    @words_repeated = @words.map { |m| m[2..-3] }.select { |s| s =~ /.+:.+/ }.map { | m | m.split(":")[0] }
   end
 
   def ask
-    @words.each do |w|
-      puts "Give me #{w[2..-3]}."
-      @answers[w.to_sym] = gets
+    
+    @words.reject { |r| @words_repeated.include?(r[2..-3]) }.each do |w|
+      multiple = false
+      
+      if w.include?(":")
+        multiple = true
+        puts "Give me #{w[2..-3].split(":")[1]}."
+      else
+        puts "Give me #{w[2..-3]}."
+      end
+      
+      answer = gets.chomp
+      @madlib.sub!(w, answer)
+      if multiple
+        @madlib.gsub!("((" + w[2..-3].split(":")[0] + "))", answer)
+      end
+      
     end
   end
 
-  def to_s
-    @madlib
+  def display
+    puts @madlib
   end
 
 end
 
 madlib = Madlibs.new("Gift_Giving.madlib")
 
-# madlib.ask
-
-p madlib.instance_variable_get("words")
+madlib.ask
+madlib.display
