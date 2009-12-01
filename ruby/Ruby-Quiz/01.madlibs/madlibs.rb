588e31eecbd1076bcf947b38d15f29e136aa169e
Aurélien Bottazini <abottazini@gmail.com>
2009-12-01 16:30:45 +0100

    Ruby Quizz: madlib

--- ruby/Ruby-Quiz/01.madlibs/madlibs.rb
+++ ruby/Ruby-Quiz/01.madlibs/madlibs.rb
@@ -1,4 +1,25 @@
 # CGI version to play with: http://rubyquiz.com/cgi-bin/madlib.cgi
-madlib = File.read("Gift_Giving.madlib")
+class Madlibs
 
-p madlib
+  def initialize(filepath)
+    @madlib = File.read(filepath)
+    @words = @madlib.scan(/\(\([^(\)\))]+\)\)/)
+    @answers = Hash.new
+  end
+
+  def ask
+    @words.each do |w|
+      puts "Give me #{w[2..-3]}."
+      @answers[w.to_sym] = gets
+    end
+  end
+
+  def to_s
+    @madlib
+  end
+
+end
+
+madlib = Madlibs.new("Gift_Giving.madlib")
+
+madlib.ask
