be83e10156be839ac5a70f4472b679727340839e
Aurelien Bottazzini <aurelien@aurelien-eeepc.(none)>
2009-12-26 18:51:42 +0100

    ruby: working on gedcom parser

--- ruby/Ruby-Quiz/03.GEDCOM_Parser/gedcom_parser.rb
+++ ruby/Ruby-Quiz/03.GEDCOM_Parser/gedcom_parser.rb
@@ -1,3 +1,42 @@
+require 'rubygems'
+require 'builder'
+
 class GEDCOMParser
-  
+  @gedcom_lines
+
+  def initialize filepath
+    @gedcom_lines = File.readlines(filepath)
+  end
+
+  def to_xml
+    xml_buffer = String.new
+    xm = Builder::XmlMarkup.new(:target => xml_buffer,
+                                :indent => 2)
+    xm.gedcom do
+      last_depth = -1
+      @gedcom_lines.each do |l|
+        splitted_line = l.chomp.split(/ +/)
+        current_depth = splitted_line[0].to_i
+
+        current_text = String.new
+        if splitted_line[1].index('@')
+          current_tag = splitted_line[2]
+          xm.tag!(current_tag.downcase,
+                  :id => splitted_line[1])
+        else
+          current_tag = splitted_line[1]
+          splitted_line[2..splitted_line.size].each do |s|
+            current_text << s + ' '
+          end
+          current_text.chop!
+          xm.tag!(current_tag.downcase, current_text)
+        end
+
+
+
+        last_depth = current_depth
+      end
+    end
+    xml_buffer
+  end
 end
--- ruby/Ruby-Quiz/03.GEDCOM_Parser/spec/gedcom_parser_spec.rb
+++ ruby/Ruby-Quiz/03.GEDCOM_Parser/spec/gedcom_parser_spec.rb
@@ -7,19 +7,14 @@ describe GEDCOMParser do
   it "should raise an argument error without a filename" do
     lambda { GEDCOMParser.new }.should raise_error(ArgumentError)
   end
+
+  it "should raise an error with an invalid filename" do
+    lambda { GEDCOMParser.new("missing.ged") }.should raise_error
+  end
   
   it "should render a valid example xml" do
     gedcom = GEDCOMParser.new("example.ged")
-    gedcom.to_xml.should eql '<gedcom>
-    <indi id="@I1@">
-    <name> 
-      Jamis Gordon /Buck/ 
-      <surn>Buck</surn>
-      <givn>Jamis Gordon</givn>
-    </name> 
-    <sex>M</sex>
-    </indi>
-    </gedcom>'
+    gedcom.to_xml.should eql File.read("example.xml")
   end
   
 end
