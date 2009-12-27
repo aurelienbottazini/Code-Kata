aacdb5c23d0669438a8ee35cbebba79d666ec3ee
Aurelien Bottazzini <aurelien@aurelien-eeepc.(none)>
2009-12-27 21:44:57 +0100

    ruby: gedcom refactoring

--- ruby/Ruby-Quiz/03.GEDCOM_Parser/gedcom_parser.rb
+++ ruby/Ruby-Quiz/03.GEDCOM_Parser/gedcom_parser.rb
@@ -7,38 +7,36 @@ class GEDCOMParser
   end
 
   def to_xml
-    xml = REXML::Element.new('gedcom')
+    xml_root = REXML::Element.new('gedcom')
 
     stack = Array.new
-    stack.push xml
+    stack.push xml_root
     last_depth = 0
-    @gedcom_lines.each do |l|
-      splitted_line = l.chomp.split(/\s+/)
+    @gedcom_lines.each do |line|
+      splitted_line = line.chomp.split(/\s+/)
 
-      current_depth = splitted_line[0].to_i
+      line =~ /^\s*([0-9]+)\s+(@\S+@|\S+)(\s(.*))?$/ or raise "GEDCOM not properly formatted"
+      current_depth = $1.to_i
+      current_tag = $2
+      data = $4
+
       while((current_depth + 1) < stack.size) do
         stack.pop
       end
-      element = stack.last
+      xml_element = stack.last
 
       attribute = false
-      if splitted_line[1].index('@')
-        current_tag = splitted_line[2]
+      if current_tag.index('@')
+        current_tag = $4
         attribute = true
-      else
-        current_tag = splitted_line[1]
       end
-      element = element.add_element(current_tag.downcase)
-      element.add_attribute('id', splitted_line[1]) if attribute
-      data = splitted_line[2..splitted_line.size].map { |x| x + ' '}.to_s.chop
-      element.add_text(data) unless attribute
+      xml_element = xml_element.add_element(current_tag.downcase)
+      xml_element.add_attribute('id', $2) if attribute
+      xml_element.add_text(data) unless attribute
 
-      stack.push element
+      stack.push xml_element
     end
 
-
-    xml.to_s
+    xml_root.to_s
   end
-
-
 end
