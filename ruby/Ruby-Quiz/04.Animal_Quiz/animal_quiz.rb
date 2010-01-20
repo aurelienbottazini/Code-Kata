0e3cfbdac7beeb66e291bb61ec7826522ff2e7da
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2010-01-20 10:18:59 +0100

    maze from ruby learning

--- ruby/Ruby-Quiz/04.Animal_Quiz/animal_quiz.rb
+++ ruby/Ruby-Quiz/04.Animal_Quiz/animal_quiz.rb
@@ -2,16 +2,13 @@
 require 'yaml'
 
 class Node
-  attr_writer :animal, :leaf
-
-  def initialize yes_node=nil, no_node=nil
-    @yes_node = yes_node
-    @no_node = no_node
-    @leaf = true
-    @question = String.new
-    @animal = String.new
+  def initialize
+    @left = nil
+    @right = nil
   end
 
+  
+  
 end
 
 animal_tree = nil
@@ -19,10 +16,4 @@ if File.exists?('animals.yml')
   animal_tree = YAML.load(File.read('animals.yml'))
 else
   animal_tree = Node.new
-  animal_tree.animal = 'Elephant'
-end
-
-puts 'Think of an animal...'
-while true
-  animal_tree.ask
 end
