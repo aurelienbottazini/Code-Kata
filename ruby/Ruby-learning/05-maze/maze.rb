9ee5bf2921d59e3bf0444b16feec8a518ef0c3d2
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2010-01-21 16:49:39 +0100

    finding the shortest path possible for maze

--- ruby/Ruby-learning/05-maze/maze.rb
+++ ruby/Ruby-learning/05-maze/maze.rb
@@ -10,21 +10,26 @@ class Maze
   def initialize maze
     # converting maze string to a two dimensional array for easier navigation
     @maze = maze.split(/\n/).map!{|line| line.chars.to_a }
+    @steps = Array.new
   end
 
   def solvable?
-    return find_exit(find_entrance, 0)
+    return find_exit(find_entrance, 0, dup_maze(@maze))
   end
 
   def steps
-    
-    @steps = 0
-    solvable?
 
-    return @steps
+    solvable?
+    return (@steps.size > 0 ) ? @steps.min : 0
   end
 
   private
+
+  def dup_maze maze
+    new_maze = Array.new
+    maze.each { |maze_element| new_maze << maze_element.dup }
+    return new_maze
+  end
 
   def find_entrance
 
@@ -39,77 +44,72 @@ class Maze
   end
 
   def is_exit?(position)
-    get_char(position) == EXIT ? true : false
+    get_char(position, @maze) == EXIT ? true : false
   end
 
-  def already_tried?(position)
-    get_char(position) == MARK ? true : false
+  def already_tried?(position, maze)
+    get_char(position, maze) == MARK ? true : false
   end
 
-  def mark_as_tried(position)
-    set_char(position, MARK)
+  def mark_as_tried(position, maze)
+    set_char(position, MARK, maze)
   end
 
-  # Recursively tries to find an exit and also sets the number of
+  # Recursively tries to find exits and also sets the number of
   # steps upon success in @steps
-  def find_exit(position, steps)
-
-    if already_tried?(position)
-      return false
-    end
-
+  def find_exit(position, steps, maze)
+    
     if is_exit?(position)
-      @steps = steps
-      return true
+      @steps << steps
     end
-
-    mark_as_tried(position)
-
+    
+    mark_as_tried(position, maze)
+    
     left = Position.new(position.row, position.column - 1)
-    if movable?(left)
-      if find_exit(left, steps + 1)
-        return true
-      end
+    if movable?(left, maze)
+      find_exit(left, steps + 1, dup_maze(maze))
     end
-
-     right = Position.new(position.row, position.column + 1)
-    if movable?(right)
-      if find_exit(right, steps + 1)
-        return true
-      end
+        
+    right = Position.new(position.row, position.column + 1)
+    if movable?(right, maze)
+      find_exit(right, steps + 1, dup_maze(maze))
     end
-
+    
     up = Position.new(position.row - 1, position.column)
-    if movable?(up)
-      if find_exit(up, steps + 1)
-        return true
-      end
+    if movable?(up, maze)
+      find_exit(up, steps + 1, dup_maze(maze))
     end
-
+    
     down = Position.new(position.row + 1, position.column)
-    if movable?(down)
-      if find_exit(down, steps + 1)
-        return true
-      end
+    if movable?(down, maze)
+      find_exit(down, steps + 1, dup_maze(maze))
     end
 
-    return false
+    return (@steps.size > 0) ? true : false
   end
 
-   def movable?(position)
-    if get_char(position) != WALL &&
-        get_char(position) != MARK
+  def movable?(position, maze)
+    if get_char(position, maze) != WALL &&
+        get_char(position, maze) != MARK
       return true
     end
 
     return false
   end
 
-   def get_char position
-    @maze[position.row][position.column]
+  def get_char position, maze
+    maze[position.row][position.column]
   end
 
-  def set_char position, char
-    @maze[position.row][position.column] = char
+  def set_char position, char, maze
+    maze[position.row][position.column] = char
   end
 end
+
+MAZE7 = %{##############
+#A           #
+######## ## ##
+#B           #
+##############}
+
+Maze.new(MAZE7).steps
--- ruby/Ruby-learning/05-maze/maze_test.rb
+++ ruby/Ruby-learning/05-maze/maze_test.rb
@@ -46,19 +46,56 @@ MAZE3 = %{#####################################
 #####################################}
 # Maze 3 should FAIL
 
+MAZE4 = %{##########
+# # #A   #
+# # ######
+###B######
+# # #    #
+##########}
+
+MAZE5 = %{##############
+#A    B      #
+##############}
+
+MAZE6 = %{##############
+#A           #
+######## #####
+#B           #
+##############}
+
+MAZE7 = %{##############
+#A           #
+######## ## ##
+#B           #
+##############}
+
+MAZE8 = %{##############
+#A           #
+########### ##
+#C           #
+##############}
+
+
+
 class MazeTest < Test::Unit::TestCase
   def test_good_mazes
     assert_equal true, Maze.new(MAZE1).solvable?
     assert_equal true, Maze.new(MAZE2).solvable?
+    assert_equal true, Maze.new(MAZE5).solvable?
+    assert_equal true, Maze.new(MAZE6).solvable?
+    assert_equal true, Maze.new(MAZE7).solvable?
   end
 
   def test_bad_mazes
     assert_equal false, Maze.new(MAZE3).solvable?
+    assert_equal false, Maze.new(MAZE4).solvable?
+    assert_equal false, Maze.new(MAZE8).solvable?
   end
 
   def test_maze_steps
     assert_equal 44, Maze.new(MAZE1).steps
     assert_equal 75, Maze.new(MAZE2).steps
     assert_equal 0, Maze.new(MAZE3).steps
+    assert_equal 16, Maze.new(MAZE7).steps
   end
 end
