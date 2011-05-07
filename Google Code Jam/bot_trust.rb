f27f14f5fe312226df39cb7b24ecea7a9be48955
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2011-05-07 11:52:26 +0200

    saving

--- Google Code Jam/bot_trust.rb
+++ Google Code Jam/bot_trust.rb
@@ -15,21 +15,43 @@ class BotTrust
     n_terms = n_terms.scan(/[OB] [0-9]+/)
 
     o_position = 1
+    robot_time_cumul = 0
     b_position = 1
-
+    time_required = 0
+    previous_robot = n_terms[0].split(' ')[0]
     n_terms.each do |n_term|
       n_term_robot, n_term_position = n_term.split(' ')
       n_term_position = n_term_position.to_i
 
       current_position = n_term_robot == 'O' ? o_position : b_position
-      time_required = time_required + time_to_move_and_push_button(current_position, n_term_position)
+      time_to_move = BotTrust.time_to_move(n_term_position, current_position)
+      current_position = n_term_position
+      if n_term_robot == 'O'
+        o_position = current_position
+      else
+        b_position = current_position
+      end
+
+      if n_term_robot != previous_robot
+        if robot_time_cumul > time_to_move
+          time_required = time_required + robot_time_cumul
+        else
+          time_required = time_required + time_to_move + 1
+        end
+        robot_time_cumul = 0
+      else
+        robot_time_cumul = robot_time_cumul + time_to_move + 1
+      end
+
+      previous_robot = n_term_robot
     end
+    time_required = time_required + robot_time_cumul
 
     time_required
   end
 
-  def self.time_to_move_and_push_button old_position, new_position
-    (old_position - new_position).abs + 1
+  def self.time_to_move old_position, new_position
+    (old_position - new_position).abs
   end
 
   # Each test case consists of a single line beginning with a positive
--- Google Code Jam/spec/bot_trust_spec.rb
+++ Google Code Jam/spec/bot_trust_spec.rb
@@ -17,13 +17,14 @@ describe BotTrust do
 
   it "should get the correct result for sample" do
     @bt.get_time_required('4 O 2 B 1 B 2 O 4').should eql 6
-    # @bt.get_time_required([5,8], [100]).should eql 100
-    # @bt.get_time_required([], [2,1]).should eql 4
+    # @bt.get_time_required('3 O 5 O 8 B 100').should eql 100
+    # @bt.get_time_required('2 B 2 B 1').should eql 4
   end
 
-  it "should give a correct move and push button time" do
-    BotTrust.time_to_move_and_push_button(3, 5).should eql 3
-    BotTrust.time_to_move_and_push_button(3, 3).should eql 1
+  it "should give a correct move time" do
+    BotTrust.time_to_move(3, 5).should eql 2
+    BotTrust.time_to_move(3, 3).should eql 0
+    BotTrust.time_to_move(2, 1).should eql 1
   end
   # it "should get the correct output" do
   #   @bt.find_sequence
