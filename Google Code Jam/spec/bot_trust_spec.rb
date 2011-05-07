5a6bd7d634a9c2b0cbd703128023094535a25a78
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2011-05-07 08:01:35 +0200

    setting up test

--- Google Code Jam/spec/bot_trust_spec.rb
+++ Google Code Jam/spec/bot_trust_spec.rb
@@ -6,11 +6,9 @@ describe BotTrust do
   it "should initialize" do
     BotTrust.new
   end
+  # it "should get the correct result for sample" do
 
-  it "should get the correct result for sample" do
-
-  end
-
+  # end
 end
 
 
--- Google Code Jam/spec/spec_helper.rb
+++ Google Code Jam/spec/spec_helper.rb
@@ -1,8 +1,11 @@
 $:.unshift File.expand_path('./')
 require 'rubygems'
 require 'rspec'
-
+require 'fileutils'
 RSpec.configure do |config|
 
+  config.after :each do
+    FileUtils.rm_rf('./tmp/.')
+  end
 
 end
