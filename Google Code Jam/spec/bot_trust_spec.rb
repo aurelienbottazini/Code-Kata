5e25e4c980a05dc53f46129cb9766d794b799fb9
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2011-05-07 08:02:03 +0200

    setting up test

--- Google Code Jam/spec/bot_trust_spec.rb
+++ Google Code Jam/spec/bot_trust_spec.rb
@@ -6,9 +6,10 @@ describe BotTrust do
   it "should initialize" do
     BotTrust.new
   end
-  # it "should get the correct result for sample" do
 
-  # end
+ it "should get the correct result for sample" do
+
+ end
 end
 
 
