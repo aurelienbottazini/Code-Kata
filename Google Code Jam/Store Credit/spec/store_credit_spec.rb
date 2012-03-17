0e33c74025f859162335bc32dc6b1c54d02de1ba
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2012-03-17 18:53:41 +0100

    store credit first try

--- Google Code Jam/Store Credit/spec/store_credit_spec.rb
+++ Google Code Jam/Store Credit/spec/store_credit_spec.rb
@@ -1,5 +1,22 @@
-describe StoreCredit do
-  it "should initialize" do
+require './store_credit.rb'
 
+describe StoreCredit do
+  context "first example" do
+    subject { StoreCredit.new(100, 3, [5, 75, 25]) }
+    its(:credit) { should == 100}
+    its(:number_of_available_items) { should == 3 }
+    its(:items_list) { should == [5, 75, 25]}
+    its(:objects_to_buy) { should == [2, 3]}
   end
+
+  context "second example" do
+    subject { StoreCredit.new(200, 7, [150, 24, 79, 50, 88, 345, 3]) }
+    its(:objects_to_buy) { should == [1, 4]}
+  end
+
+  context "third example" do
+    subject { StoreCredit.new(8, 8, [2, 1, 9, 4, 4, 56, 90, 3]) }
+    its(:objects_to_buy) { should == [4, 5]}
+  end
+
 end
--- Google Code Jam/Store Credit/store_credit.rb
+++ Google Code Jam/Store Credit/store_credit.rb
@@ -1,3 +1,37 @@
 class StoreCredit
+  attr_reader :credit, :number_of_available_items, :items_list
 
+  def initialize credit, number_of_available_items, items_list
+    @credit, @number_of_available_items, @items_list = credit, number_of_available_items, items_list
+    @solution
+  end
+
+  def objects_to_buy
+    add_item([], @items_list)
+    to_buy_index_list = Array.new
+    @solution.each do |value|
+      to_buy_index_list << value[1] + 1
+    end
+
+    to_buy_index_list.sort
+  end
+
+  private
+
+  def add_item choosed_items, remaining_items
+    current_sum = choosed_items.inject(0) { |sum, x| sum+=x[0]}
+    if current_sum == @credit
+      @solution = choosed_items
+    else
+      remaining_items.each_with_index do |item, index|
+        cloned_remaining_items = Marshal.load(Marshal.dump(remaining_items))
+        cloned_remaining_items.delete_at(index)
+        cloned_choosed_items = Marshal.load(Marshal.dump(choosed_items))
+        cloned_choosed_items << [item, index]
+        if cloned_choosed_items.size < 3
+          add_item(cloned_choosed_items, cloned_remaining_items)
+        end
+      end
+    end
+  end
 end
