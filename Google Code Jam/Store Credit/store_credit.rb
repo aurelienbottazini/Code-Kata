59b0db84c2018d3f2bc3fd2795bc5066b920a2bb
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2012-03-17 20:47:52 +0100

    saving before refactor

--- Google Code Jam/Store Credit/store_credit.rb
+++ Google Code Jam/Store Credit/store_credit.rb
@@ -4,13 +4,21 @@ class StoreCredit
   def initialize credit, number_of_available_items, items_list
     @credit, @number_of_available_items, @items_list = credit, number_of_available_items, items_list
     @solution
+    @should_stop = false
   end
 
   def objects_to_buy
     add_item([], @items_list)
     to_buy_index_list = Array.new
+
     @solution.each do |value|
-      to_buy_index_list << value[1] + 1
+      good_index = 0
+      @items_list.each_with_index do |item, index|
+        if item == value && !to_buy_index_list.include?(index+1)
+          good_index = index+1
+        end
+      end
+      to_buy_index_list << good_index
     end
 
     to_buy_index_list.sort
@@ -19,17 +27,20 @@ class StoreCredit
   private
 
   def add_item choosed_items, remaining_items
-    current_sum = choosed_items.inject(0) { |sum, x| sum+=x[0]}
+    current_sum = choosed_items.inject(0) { |sum, x| sum+=x}
     if current_sum == @credit
       @solution = choosed_items
+      @should_stop = true
     else
       remaining_items.each_with_index do |item, index|
         cloned_remaining_items = Marshal.load(Marshal.dump(remaining_items))
         cloned_remaining_items.delete_at(index)
         cloned_choosed_items = Marshal.load(Marshal.dump(choosed_items))
-        cloned_choosed_items << [item, index]
+        cloned_choosed_items << item
         if cloned_choosed_items.size < 3
-          add_item(cloned_choosed_items, cloned_remaining_items)
+          if @should_stop == false
+            add_item(cloned_choosed_items, cloned_remaining_items)
+          end
         end
       end
     end
