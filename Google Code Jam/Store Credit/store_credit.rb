88180bde74cc0dbddc5961ef604d245afe6698f4
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2012-03-17 21:00:08 +0100

    speedy version of store_credit.rb

--- Google Code Jam/Store Credit/store_credit.rb
+++ Google Code Jam/Store Credit/store_credit.rb
@@ -8,41 +8,14 @@ class StoreCredit
   end
 
   def objects_to_buy
-    add_item([], @items_list)
-    to_buy_index_list = Array.new
-
-    @solution.each do |value|
-      good_index = 0
-      @items_list.each_with_index do |item, index|
-        if item == value && !to_buy_index_list.include?(index+1)
-          good_index = index+1
-        end
-      end
-      to_buy_index_list << good_index
-    end
-
-    to_buy_index_list.sort
-  end
-
-  private
-
-  def add_item choosed_items, remaining_items
-    current_sum = choosed_items.inject(0) { |sum, x| sum+=x}
-    if current_sum == @credit
-      @solution = choosed_items
-      @should_stop = true
-    else
-      remaining_items.each_with_index do |item, index|
-        cloned_remaining_items = Marshal.load(Marshal.dump(remaining_items))
-        cloned_remaining_items.delete_at(index)
-        cloned_choosed_items = Marshal.load(Marshal.dump(choosed_items))
-        cloned_choosed_items << item
-        if cloned_choosed_items.size < 3
-          if @should_stop == false
-            add_item(cloned_choosed_items, cloned_remaining_items)
-          end
+    item_list_copy = Marshal.load( Marshal.dump(@items_list ) )
+    @items_list.each_with_index do |item, index|
+      item_list_copy.each_with_index do |copy_item, copy_index|
+        if index != copy_index && (item + copy_item) == @credit
+          return [index+1, copy_index+1].sort
         end
       end
     end
   end
+
 end
