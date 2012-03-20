3f120d4a4e62d2154b18d08add558b84c92b9a0e
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2012-03-20 13:29:44 +0100

    quicker alien_language

--- Google Code Jam/Alien Language/alien_language.rb
+++ Google Code Jam/Alien Language/alien_language.rb
@@ -9,13 +9,32 @@ end
   pattern = gets
   tokens = pattern.scan(/\(?[a-z]+\)?/)
 
-  p tokens
+  new_tokens = Array.new
+  tokens.each do |token|
+    if token =~ /^\(/
+      token = token[1..-2]
+      new_tokens << token.scan(/./)
+    else
+      token.each_char do |char|
+        new_tokens << [char]
+      end
+    end
+  end
 
   matching_words = 0
   words.each do |word|
     matching = true
+    index = 0
     word.each_char do |char|
+      if !new_tokens[index].include?(char)
+        matching = false
+        break
       end
+      index +=1
+    end
+
+    if matching == true
+      matching_words += 1
     end
   end
 
@@ -25,5 +44,6 @@ end
 
 
 
-  puts "Case ##{case_number}: #{}"
+
+  puts "Case ##{case_number}: #{matching_words}"
 end
