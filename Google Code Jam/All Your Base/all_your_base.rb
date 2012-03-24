c578afbd52ecd7b86cbb547a3f36c7626b50a4f2
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2012-03-25 00:14:24 +0100

    All Your Base for Google Code jam

---
+++ Google Code Jam/All Your Base/all_your_base.rb
@@ -0,0 +1,34 @@
+def next_map_number map_number
+  if map_number == 0
+    map_number = 2
+  else
+    map_number += 1
+  end
+end
+1.upto(gets.to_i) do |case_number|
+
+  message = gets.chomp
+
+  chars_mapping = Hash.new
+  chars_mapping[message[0]] = 1
+
+  map_number = 0
+  message.each_char do |char|
+    if !chars_mapping.has_key?(char)
+      chars_mapping[char] = map_number
+      map_number = next_map_number map_number
+    end
+  end
+
+  base = chars_mapping.size
+  base = 2 if base < 2
+  smallest_time = 0
+  index = message.size - 1
+  message.each_char do |char|
+
+    smallest_time += chars_mapping[char] * (base ** index)
+    index = index - 1
+  end
+
+  puts "Case ##{case_number}: #{smallest_time}"
+end
